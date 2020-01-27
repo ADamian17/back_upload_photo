3998236001f549af57a2ec37054d9c8bd4e8cccf
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2020-01-27 10:12:17 -0800

    event.js unhandle err needs to get fix

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const db = require('../models');
+const mongoose = require('mongoose');
+
 
 // all Users
 const index = (req, res) => {
@@ -17,47 +19,49 @@ const index = (req, res) => {
   });
 };
 
+
 // create user
 const createUser = (req, res) => {
-
-  if (
-    !req.body.firstName ||
-    !req.body.lastName
-    // !req.body.profilePhoto
-  ) {
-    return res.status(400).json({
-      status: 400,
-      message:
-        "Please enter your name"
-    });
-  }
+  // if (
+  //   !req.body.firstName ||
+  //   !req.body.lastName ||
+  //   !req.body.profilePhoto
+  // ) {
+  //   return res.status(400).json({
+  //     status: 400,
+  //     message:
+  //       "Please enter your name"
+  //   });
+  // }
   db.User.findOne({ name: req.body.name }, (err, foundUser) => {
+    console.log(`This is foundUser ${foundUser}`)
     if (err) return res.status(500).json({
         status: 500,
-        message: "Something went wrong. Please try again"
-      });
-      
-    if (foundUser) res.status(400).json({
-        status: 400,
-        message: "Something went wrong. Please try again stops here"
+        message: "Something went wrong. Please try again 1"
       });
 
-      const newUser = {
+    if (foundUser) res.status(400).json({
+        status: 400,
+        message: "Something went wrong. Please try again 2"
+      });
+
+      const newUser = new db.User ({
+        _id: new mongoose.Types.ObjectId(),
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-      };
+        profileImage: req.file.path
+      });
+
       db.User.create(newUser, (err) => {
-        console.log(newUser)
-        if (err)
-          return res.status(500).json({
+        console.log(`This is newUser ${newUser}`)
+        if (err) return res.status(500).json({
             status: 500,
-            message: "Something went wrong. Please try again"
+            message: "Something went wrong. Please try again 3"
           });
         res.status(201).json({ status: 201, message: "success" });
       });
   });
 };
-
 
 // SHow One User
 const profile = (req, res) => {
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const db = require('../models');
 
 // Multer Set up
@@ -15,42 +14,14 @@ filename: function (req, file, cb) {
 })
 
 const upload = multer({ storage: storage })
+
 // Controllers
 const ctrl = require('../controllers')
 
-
 // User Routes
 router.get('/', ctrl.user.index);
-// router.post('/createuser', ctrl.user.createUser);
+router.post('/createuser', upload.single('profileImage'), ctrl.user.createUser);
 router.get('/:id', ctrl.user.profile);
-
-router.post('/uploadphoto', upload.single('profileImage'), ( req, res ) => {
-  const uploadPhoto = new db.User ({
-    _id: new mongoose.Types.ObjectId(),
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    profileImage: req.file.path
-  })
-  uploadPhoto
-  .save()
-  .then( res => console.log(res))
-  .catch(err => console.log(err))
-  res.status(200).json({
-    status: 200,
-    message: "susses"
-  });
-});
-
-// router.post('/upload',function(req, res) {
-//   upload(req, res, function (err) {
-//          if (err instanceof multer.MulterError) {
-//              return res.status(500).json(err)
-//          } else if (err) {
-//              return res.status(500).json(err)
-//          }
-//     return res.status(200).send(req.file)
-//   })
-// });
 
 
 module.exports = router;
